bc01345a14bb0c5dde222e5f89cecdcd02004d5d
viktor111 <swifrorlilko@gmail.com>
2020-06-05 02:13:54 +0300

    inital

---
+++ ConsoleWeatherCSharp/ClientData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleWeatherCSharp
+{
+    public class ClientData
+    {
+        private string _city;
+        private string _key = "bc5f3c4ea12f8a20f5d49ba45071ac67";
+
+        public ClientData(string city)
+        {
+            _city = city;
+        }
+
+        public Uri ApiData()
+        {
+            Uri apiString = new Uri($"http://api.openweathermap.org/data/2.5/weather?q={_city}&appid={_key}");
+
+            return apiString;
+        }
+        
+    }
+}
---
+++ ConsoleWeatherCSharp/WeatherClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Web;
+
+namespace ConsoleWeatherCSharp
+{
+    public class WeatherClient : IWebRequestCreate
+    {
+        public WeatherClient()
+        {
+        }
+
+        public WebRequest Create(Uri uri)
+        {
+            WebRequest request = WebRequest.Create(uri);
+
+            return request;
+        }
+
+
+        public HttpWebResponse Response(WebRequest webRequest)
+        {          
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+                return response;
+            }
+            catch(WebException e)
+            {
+                Console.WriteLine("[-] Input city");
+
+                return null;
+            }            
+            
+        }
+    }
+}
